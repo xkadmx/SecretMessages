e143c1c1e199ba60d9bca4aee87fc968214f6cbd
xkadmx <zuzanna.rosinska@yahoo.com>
2019-02-20 21:57:03 +0100

    Added:
    - encode() created;
    - btn setOnClickListener;
    - int key parsing from text (number) to string;
    - String message parsing from text (editable value) to string;
    - encode() arguments set : message and key
    - txtOut.setText(output);
    
    Signed-off-by: xkadmx <zuzanna.rosinska@yahoo.com>

--- app/src/main/java/com/example/secretmessages/MainActivity.java
+++ app/src/main/java/com/example/secretmessages/MainActivity.java
@@ -20,6 +20,38 @@ public class MainActivity extends AppCompatActivity {
     SeekBar sb;
     Button btn;
 
+    public String encode (String message, int keyVal) {
+        String output = "";
+        char key = (char) keyVal;
+        for (int x = 0; x < message.length(); x++) {
+            char input = message.charAt(x);
+            if (input >= 'A' && input <= 'Z') {
+                input += key;
+                if (input > 'Z')
+                    input -= 26;
+                if (input < 'A')
+                    input += 26;
+            } else if (input >= 'a' && input <= 'z') {
+                input += key;
+                if (input > 'z')
+                    input -= 26;
+                if (input <= 'a')
+                    input += 26;
+            } else if (input >= '0' && input <= '9') {
+                input += (keyVal % 10);
+                if (input > '9')
+                    input -= 10;
+                if (input < '0')
+                    input += 10;
+            }
+            output += input;
+        }
+        return output;
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +64,15 @@ public class MainActivity extends AppCompatActivity {
         txtOut = (TextView) findViewById(R.id.txtOut);
         sb = (SeekBar) findViewById(R.id.sb);
         btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                int key = Integer.parseInt(txtKey.getText().toString());
+                String message = txtIn.getText().toString();
+                String output = encode(message, key);
+                txtOut.setText(output);
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
